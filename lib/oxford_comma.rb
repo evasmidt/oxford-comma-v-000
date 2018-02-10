e354d8f52865bdfcd29cdc48e73a588344617da7
Eva Smidt <shevangeline@gmail.com>
2018-02-10 19:10:00 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
 if array.length == 2
   return "#{array[0]} and #{array[2]}"
-elsif 2 < array.length
+else 2 < array.length
   array[-1].insert(0, "and ")
 end
 array.join(", ")
