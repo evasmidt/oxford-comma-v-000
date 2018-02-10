1695ff2950b2d571a2e0990ae6fbadcbfdb8a4e8
Eva Smidt <shevangeline@gmail.com>
2018-02-10 19:10:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
 if array.length == 2
-  return "#{array[0]} and #{array[2]}"
+  return "#{array[0]} and #{array[1]}"
 else 2 < array.length
   array[-1].insert(0, "and ")
 end
